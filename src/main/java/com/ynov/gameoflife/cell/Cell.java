e9aa2d40249c00d60ccdff6f968e9f7378df1f03
rbeneteau <r.beneteau@outlook.fr>
2017-01-20 23:08:52 +0100

    Add constants for ALIVE, DEAD and NEW BORN Cells representation

--- src/main/java/com/ynov/gameoflife/cell/Cell.java
+++ src/main/java/com/ynov/gameoflife/cell/Cell.java
@@ -2,6 +2,32 @@ package com.ynov.gameoflife.cell;
 
 public interface Cell {
 
+
+    /*
+     * ---------------------------------
+     *		Constantes
+     * ---------------------------------
+     */
+
+    /*
+     * Dans une interface, il n'est pas possible d'avoir des attributs d'instance, les valeurs ci-dessous sont donc toutes des variables STATIC et FINAL = des constantes.
+     * Puisqu'il n'y aurait pas d'intérêt d'avoir des constantes qui ne peuvent être utilisées nul part, ces variables sont donc PUBLIC implicitement.
+     * Note: Il est donc possible de se passer des mot-clés "public static final" puisqu'ils sont implicites.
+     */
+
+    /** Representation d'une cellule morte. */
+    String DEAD_CELL_REPRESENTATION 	= "-";
+    /** Representation d'une cellule juste-née. */
+    String NEWBORN_CELL_REPRESENTATION = "0";
+    /** Representation d'une cellule vivante. */
+    String ALIVE_CELL_REPRESENTATION 	= "+";
+
+    /*
+     * ---------------------------------
+     *		Méthodes
+     * ---------------------------------
+     */
+
     /*
      * Dans une interface, on définie des comportements qui doivent obligatoirement être re-définie dans les classes qui implémentent cette interface.
      * Ces méthodes sont donc toutes ABSTRACT et PUBLIC.
@@ -19,9 +45,9 @@ public interface Cell {
 
     /**
      * Representation de la cellule sous forme de chaine de charactères: <br>
-     * 			- Cellule morte: - <br>
-     * 			- Cellule vivante: + <br>
-     * 			- Cellule juste-née: 0 <br>
+     * 			- Cellule morte: {@link Cell#DEAD_CELL_REPRESENTATION} <br>
+     * 			- Cellule vivante: {@link Cell#ALIVE_CELL_REPRESENTATION} <br>
+     * 			- Cellule juste-née: {@link Cell#NEWBORN_CELL_REPRESENTATION} <br>
      *
      * @return Une chaine de charactère representante la cellule.
      */
