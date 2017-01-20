3c3a4ad49531d073fce6389968e6f9b5dc01138a
rbeneteau <r.beneteau@outlook.fr>
2017-01-20 23:48:51 +0100

    Generate a new random world

--- src/main/java/com/ynov/gameoflife/Launcher.java
+++ src/main/java/com/ynov/gameoflife/Launcher.java
@@ -4,14 +4,24 @@ import com.ynov.gameoflife.cell.*;
 
 public class Launcher {
     public static void main(String[] args) {
+
+        System.out.println("---------- Exemple avec map aléatoire ----------");
+        World randomWorld = new World(5, 5);
+
+        // Nouvelles générations
+        for(int i = 0; i < 4; i++) {
+            System.out.println(randomWorld); // Grâce à la surcharge de toString() de la classe World
+            randomWorld.newGeneration();
+        }
+
+        System.out.println("---------- Exemple avec map ppt ----------");
         Cell[][] pptMap = new Cell[][]{{new AliveCell(), new DeadCell(), new AliveCell(), new AliveCell(), new DeadCell(), new AliveCell(), new AliveCell(), new DeadCell(), new AliveCell(), new DeadCell()}, {new DeadCell(), new AliveCell(), new DeadCell(), new AliveCell(), new DeadCell(), new DeadCell(), new AliveCell(), new DeadCell(), new AliveCell(), new DeadCell()}, {new AliveCell(), new DeadCell(), new DeadCell(), new DeadCell(), new AliveCell(), new AliveCell(), new DeadCell(), new DeadCell(), new AliveCell(), new DeadCell()}, {new AliveCell(), new DeadCell(), new DeadCell(), new DeadCell(), new DeadCell(), new DeadCell(), new AliveCell(), new AliveCell(), new AliveCell(), new DeadCell()}, {new DeadCell(), new DeadCell(), new AliveCell(), new AliveCell(), new DeadCell(), new DeadCell(), new AliveCell(), new DeadCell(), new AliveCell(), new DeadCell()}, {new DeadCell(), new DeadCell(), new AliveCell(), new DeadCell(), new AliveCell(), new DeadCell(), new DeadCell(), new DeadCell(), new AliveCell(), new DeadCell()}, {new AliveCell(), new AliveCell(), new AliveCell(), new AliveCell(), new AliveCell(), new AliveCell(), new DeadCell(), new AliveCell(), new AliveCell(), new AliveCell()}, {new AliveCell(), new DeadCell(), new AliveCell(), new AliveCell(), new DeadCell(), new DeadCell(), new DeadCell(), new DeadCell(), new DeadCell(), new AliveCell()}, {new AliveCell(), new AliveCell(), new AliveCell(), new AliveCell(), new AliveCell(), new DeadCell(), new DeadCell(), new AliveCell(), new AliveCell(), new DeadCell()}, {new AliveCell(), new DeadCell(), new DeadCell(), new DeadCell(), new DeadCell(), new DeadCell(), new AliveCell(), new DeadCell(), new AliveCell(), new DeadCell()}};
 
         World world = new World(pptMap);
-        System.out.println(world);
 
         for(int i=0; i<6; i++) {
-            world.newGeneration();
             System.out.println(world);
+            world.newGeneration();
         }
     }
 }
