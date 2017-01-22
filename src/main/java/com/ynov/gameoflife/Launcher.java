063598bb4cef8f2373be755e94b5b6415a1a08be
rbeneteau <r.beneteau@outlook.fr>
2017-01-22 21:26:44 +0100

    Add user input for the world size

--- src/main/java/com/ynov/gameoflife/Launcher.java
+++ src/main/java/com/ynov/gameoflife/Launcher.java
@@ -2,11 +2,24 @@ package com.ynov.gameoflife;
 
 import com.ynov.gameoflife.cell.*;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Launcher {
     public static void main(String[] args) {
 
+        // Lecture nombre de colonnes
+        System.out.println("Please enter the number of columns of the world: ");
+        int nbCol = askUserForAnInteger();
+
+
+        // Lecture nombre de lignes
+        System.out.println("\nPlease enter the number of rows of the world: ");
+        int nbRow = askUserForAnInteger();
+
+
         System.out.println("---------- Exemple avec map aléatoire ----------");
-        World randomWorld = new World(5, 5);
+        World randomWorld = new World(nbCol, nbRow);
 
         // Nouvelles générations
         for(int i = 0; i < 4; i++) {
@@ -23,5 +36,18 @@ public class Launcher {
             System.out.println(world);
             world.newGeneration();
         }
+    }
+
+    /**
+     * Lis un entier sur l'entree standard.
+     * @return
+     * 			Entier saisi par l'utilisateur.
+     */
+    private static int askUserForAnInteger() {
+        // Création d'un scanner pour lire les entrées sur l'entrée standard (console)
+        Scanner sc = new Scanner(System.in);
+        int readInt = sc.nextInt();
+
+        return readInt;
     }
 }
